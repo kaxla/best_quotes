edc02535fde5aaf6e2bfba3c96caa8abab4bb672
kaxla <kaylas.email.address@gmail.com>
2014-02-20 16:00:59 -0800

    chapter2

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require "rulers"
 
-$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app","controllers")
+$LOAD_PATH << File.join(File.dirname(__FILE__),
+                        "..", "app",
+                        "controllers")
 require "quotes_controller"
 
 module BestQuotes
