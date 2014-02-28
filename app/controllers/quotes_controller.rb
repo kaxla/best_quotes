88aac3178cef6e85a4430904b14b3c87265491e3
kaxla <kaylas.email.address@gmail.com>
2014-02-27 18:48:26 -0800

    chapter6

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -25,7 +25,9 @@ class QuotesController < Rulers::Controller
     render :quote, :obj => m
   end
 
-  def update
-
+  def show
+    quote = FileModel.find(params["id"])
+    ua = request.user_agent
+    render_response :quote, :obj => quote, :ua => ua
   end
 end
